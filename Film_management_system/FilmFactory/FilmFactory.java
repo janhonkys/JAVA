235ddca386ee6c983ebb3e756f11040026ae2e60
janhonkys <78761684+janhonkys@users.noreply.github.com>
2024-07-18 21:46:52 +0200

    Delete Film_management_system/FilmFactory directory

--- Film_management_system/FilmFactory/FilmFactory.java
+++
@@ -1,9 +0,0 @@
-package projektPC2_designpatterns.FilmFactory;
-
-import projektPC2_designpatterns.Film.Film;
-
-// Interface for creating film objects.
-public interface FilmFactory {
-    // Method to create a film with given details.
-    Film createFilm(String name, String director, int year);
-}
